5ede881d34e71238bdb248e25dc0d7b892c81b16
Yuiko911 <101722573+Yuiko911@users.noreply.github.com>
2025-10-06 19:12:02 +0200

    added card effect

--- src/views/AlbumsView.vue
+++ src/views/AlbumsView.vue
@@ -34,10 +34,36 @@ onMounted(async () => {
 }
 
 .card {
-	text-decoration: none;
-	color: black;
-	text-align: center;
-	height: auto;
-	width: 150px;
+  display: flex;
+  color: black;
+  flex-direction: column;
+  align-items: center;
+  width: 180px;
+  font-weight: normal;
+  text-align: center;
+  text-decoration: none;
+  border: 1px solid #ddd;
+  border-radius: 0px;
+  padding: 10px;
+  background-color: #fafafa;
+  transition: transform 0.2s ease, box-shadow 0.2s ease;
+}
+
+.card:hover {
+  transform: scale(1.05);
+  box-shadow: 0 6px 12px rgba(0, 0, 0, 0.1);
+}
+
+.card img {
+  width: 150px;
+  height: 150px;
+  object-fit: cover;
+  border-radius: 8px;
+}
+
+.card p {
+  margin-top: 10px;
+  font-weight: 600;
+  font-size: 14px;
 }
 </style>
